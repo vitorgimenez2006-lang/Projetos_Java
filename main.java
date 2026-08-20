0024605855da722f69a7f58bbf3a7fe9eb19aa0f
Vitor Tasso <vitorgimenez2006@gmail.com>
2026-08-19 21:42:44 -0300

    Aprendendo guardar válores em váriaveis

--- main.java
+++ main.java
@@ -1,5 +1,16 @@
-public class main {
-   public static void main(String[] args) {
-      System.out.println("Olá, Mundo!"); //Imprime na tela a frase
-   }
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        System.out.println("Este é um programa Java simples.");
+        System.out.println("Ele imprime algumas mensagens no console.");
+
+        
+        
+        Scanner entrada = new Scanner(System.in);
+        String nome = entrada.nextLine();
+        int idade = entrada.nextInt();
+      
+        
+    }
 }
