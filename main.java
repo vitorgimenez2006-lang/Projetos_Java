8054bec9b356a2ddd3cc0c7d4a5327f731f10ed0
Vitor Tasso <vitorgimenez2006@gmail.com>
2026-08-21 20:34:33 -0300

    Aula hello word

--- main.java
+++ main.java
@@ -1,16 +1,6 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
-        System.out.println("Este é um programa Java simples.");
-        System.out.println("Ele imprime algumas mensagens no console.");
-
-        
-        
-        Scanner entrada = new Scanner(System.in);
-        String nome = entrada.nextLine();
-        int idade = entrada.nextInt();
-      
-        
+        String nome = "Aluno: Vitor";
+        System.out.println(nome);
     }
 }
