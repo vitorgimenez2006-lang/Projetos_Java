eaf09db6da30db6f226adaf569f5d0533a00d410
Vitor Tasso <vitorgimenez2006@gmail.com>
2026-08-21 21:01:52 -0300

    Caixa de perguntas java

--- main.java
+++ main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String nome = "Aluno: Vitor";
-        System.out.println(nome);
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite seu nome:");
+        String nome = entrada.nextLine();
+        System.out.println("Olá, " + nome);
+        entrada.close();
     }
 }
